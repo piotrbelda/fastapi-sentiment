0f56458ca58911dab7f49a3a92d95de91f49e222
piotrek <piotrek.belda@wp.pl>
2021-07-29 19:22:17 +0200

    docker config

--- project/app/main.py
+++ project/app/main.py
@@ -4,7 +4,8 @@ from app.config import Settings, get_settings
 app = FastAPI()
 
 @app.get("/ping")
-def pong(settings: Settings = Depends(get_settings)):
+async def pong(settings: Settings = Depends(get_settings)):
     return {"ping": "pong!",
             "environment": settings.environment,
             "testing": settings.testing}
+    
