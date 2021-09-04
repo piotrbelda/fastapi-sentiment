ccf698fb90f4fc294dc16801a73f0fa55617f535
piotrek <piotrek.belda@wp.pl>
2021-09-04 22:51:31 +0200

    js script

--- project/app/static/script.js
+++ project/app/static/script.js
@@ -1,1 +1,15 @@
-alert('JS script attached!')
+const sentiments = document.querySelectorAll("#sentiment-wrapper");
+
+const sentimentsCopy = [...sentiments];
+
+document.getElementById("hideNegative").addEventListener('change', (e) => {
+    checked = document.getElementById("hideNegative").checked;
+    if (checked) {
+        document.getElementById('sentiments').innerHTML = "";
+    }
+    else {
+        sentimentsCopy.forEach(sentiment => {
+            document.getElementById('sentiments').appendChild(sentiment);
+        })
+    }
+})
